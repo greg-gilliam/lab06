1a1977f5ca4f472ea61d0352302ae5e4308e7afd
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-11 14:43:39 -0700

    added a lot of stuff

---
+++ data/desserts.js
@@ -0,0 +1,37 @@
+// module.exports = [
+//   {
+//     name: 'bessie',
+//     cool_factor: 3,
+//   },
+//   {
+//     name: 'jumpy',
+//     cool_factor: 4,
+//   },
+//   {
+//     name: 'spot',
+//     cool_factor: 10,
+//   }
+// ];
+
+const desserts = [
+  {
+    'id': 1,
+    name: 'Lemon Meringue',
+    icing: false,
+    type: 'pie'
+  },
+  {
+    'id': 2,
+    name: 'Carrot Cake',
+    icing: true,
+    type: 'cake'
+  },
+  {
+    'id': 3,
+    name: 'Devils Food Cake',
+    icing: true,
+    type: 'cake',
+  }
+];
+
+module.exports = desserts;
